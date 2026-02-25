eccb2f937e7aad9de215f1dfe0d08826098698eb
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2026-02-25 14:43:12 +0200

    Exclude Windows Server updates: KB5070881, KB5072359

--- config.py
+++ config.py
@@ -24,8 +24,10 @@ windows_versions_unsupported = {
 
 updates_unsupported = {
     # Windows Server only.
+    'KB5070881',
     'KB5070882',
     'KB5070883',
+    'KB5072359',
 
     # Missing on the Update Catalog.
     'KB5071959',
